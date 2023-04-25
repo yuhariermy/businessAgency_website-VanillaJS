e0e53a460735de8ccdd8fc567b322aeadaf8f1ae
yuhariermy <yuhariermy@gmail.com>
2023-04-25 16:46:46 +0700

    fixing header behaviour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,35 @@
-'use strict'
+"use strict";
 
 // ======ADD EVENT LISTENER ON MULTIPLE ELEMENTS==========
 
-const addEventOnListeners = function (elements, eventType, callback) {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].addEventListeners(eventType, callback);
-    }
-}
-
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
 
 // ==============NAVBAR TOOGLER FOR MOBILE===================
 
-const navbar = document.querySelector("[data-navbar]")
-const navTooglers = document.querySelectorAll("[data-nav-toogler]")
-const overlay = document.querySelector("[data-overlay]")
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
 
-const toogleNavbar = function() {
-    navbar.classList.toogle("active");
-    overlay.classList.toogle("active");
-    document.body.classList.toogle("nav-active");
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
 }
 
-addEventOnListeners(navTooglers, "click", toogleNavbar)
-
+addEventOnElements(navTogglers, "click", toggleNavbar);
 
 // ============HEADER ACTIVE WHEN SCROLLDOWN================
 
-const header = document.querySelector("[data-header]")
-window.addEventListener("scroll", function () {
-    if(window.scrollY > 100) {
-        header.classList.add("active")
-    } else {
-        header.classList.remove("active")
-    }
-})
+const header = document.querySelector("[data-header]");
 
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
